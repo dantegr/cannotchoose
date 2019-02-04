e1dac8a7c2e98059528152fe443601a5bc882130
Pantelis <tampantelis@gmail.com>
2019-02-04 18:20:13 +0200

    JSX tested

--- src/app.js
+++ src/app.js
@@ -1,7 +1,35 @@
 console.log('app.js is running');
 
+var app = {
+  title: 'CanNotChoose',
+  subtitle: 'Let your computer choose for you!'
+};
 
-var template = <h1>change?</h1>;
+
+var template = (
+  <div>
+    <h1>{app.title}</h1>
+    <p>{app.subtitle}</p>
+    <ol>
+      <li>Item one</li>
+      <li>Item two</li>
+    </ol>
+  </div>
+);
+
+var user = {
+  name: 'Pantelis',
+  age: 32,
+  location: 'Thessaloniki'
+};
+
+var templateTwo = (
+<div>
+  <h1>{user.name}</h1>
+  <p>Age: {user.age}</p>
+  <p>Location: {user.location}</p>
+</div>
+);
 
 var appRoot = document.getElementById('app');
 
