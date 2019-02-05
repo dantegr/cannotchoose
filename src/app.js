efbdaf6ceb3779f7304e7045b7d8e96441813cac
Pantelis <tampantelis@gmail.com>
2019-02-05 20:12:48 +0200

    Added testin folder

--- src/app.js
+++ src/app.js
@@ -1,13 +1,13 @@
 console.log('app.js is running');
 
-var app = {
+const app = {
   title: 'CanNotChoose',
   subtitle: 'Let your computer choose for you!',
   options: ['One','Two']
 };
 
 
-var template = (
+const template = (
   <div>
     <h1>{app.title}</h1>
     {app.subtitle && <p>{app.subtitle}</p>}
@@ -19,7 +19,7 @@ var template = (
   </div>
 );
 
-var user = {
+const user = {
   name: 'Pantelis',
   age: 32,
   location: 'Thessaloniki'
@@ -31,7 +31,7 @@ function getLocation(location){
   } 
 }
 
-var templateTwo = (
+const templateTwo = (
 <div>
   <h1>{user.name ? user.name : 'Anonymous'}</h1>
   {(user.age && user.age >= 18) && <p>Age: {user.age}</p>}
@@ -40,6 +40,6 @@ var templateTwo = (
 </div>
 );
 
-var appRoot = document.getElementById('app');
+const appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
---
+++ src/playground/es6-let-const.js
@@ -0,0 +1,9 @@
+var nameVar ='Pantelis';
+nameVar = 'ffff';
+
+console.log('nameVar',nameVar);
+
+let namelet ='Pantelis';
+namelet = 'ffffjjj'
+
+console.log('nameVar',namelet);
