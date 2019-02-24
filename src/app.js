c269a39c59813f098661d65d46d9b871d94643e9
Pantelis <tampantelis@gmail.com>
2019-02-24 13:09:19 +0200

    added normalize.css

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CanNotChooseApp from './components/CanNotChooseApp';
+import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
 
