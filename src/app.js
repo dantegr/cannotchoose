a3c6c1cc7933994da053779983eafb7fc9eab420
Pantelis <tampantelis@gmail.com>
2019-02-24 12:47:29 +0200

    added sass-css to webpack

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CanNotChooseApp from './components/CanNotChooseApp';
-
+import './styles/styles.scss';
 
 
 ReactDOM.render(<CanNotChooseApp />, document.getElementById('app'));
