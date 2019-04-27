efdce44c82b0aece9993eeb22371ea007565dddc
Pantelis <tampantelis@gmail.com>
2019-04-27 11:52:58 +0300

    setup production build

--- server/server.js
+++ server/server.js
@@ -4,8 +4,10 @@ const app = express();
 
 const publicPath = path.join(__dirname, '..','public');
 
+const port = process.env.PORT || 3000;
+
 app.use(express.static(publicPath));
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log('Server is up!');
 });
